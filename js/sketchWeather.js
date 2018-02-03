61fb95bf8a1a123b2d7d2286e67d87815a33e3cb
Jakub Kondyjowski <j.kondyjowski@bizin.pl>
2018-02-03 18:53:17 +0100

    Added forecast for 3 days

--- js/sketchWeather.js
+++ js/sketchWeather.js
@@ -6,61 +6,76 @@ var curr_location, curr_country, curr_temp, angle, conditionIcon, conditionText,
 var balls=[];
 var callMethod = false;
 var cities=[];
+var forecast=[];
 
 function setup() {
   createCanvas(window.innerWidth/2, window.innerHeight/2);
+
+  initAutocomplete();
 
   wind = createVector();
 
   input = $('#locationInput');
   button = $('#showButton');
 
-  $("#locationInput").autocomplete({
-    source :function( request, response ) {
-      $.ajax({
-       url: "https://api.apixu.com/v1/search.json?key=513d8003c8b348f1a2461629162106",
-       dataType: "json",
-       data: {
-        q: request.term
-      },
-      success: function( data ) {
-        if(!data.length){
-
-          var result = [
-          {
-           label: 'No matches found', 
-           value: response.term
-         }
-         ];
-
-         response(result);
-       }
-       else {
-        response($.map(data, function(item) {
-          var arr=item.name.split(",");
-
-          return {
-            label : item.name,
-            value : arr[0]
-          };
-        }));
-      }
-
-    }
-  });
-    },
-    select: function (event, ui) {
-     $("#locationInput").val(ui.item.label);
-     $("#locationInput").attr("data-search", ui.item.value);
-     return false;
-   }
- });
-
   button.click(function(event) {
     loadCurrentWeather();
   });
 
   noLoop();
+}
+
+function initAutocomplete()
+{
+   $("#locationInput").autocomplete({
+     source :function( request, response ) {
+       $.ajax({
+        url: "https://api.apixu.com/v1/search.json?key=513d8003c8b348f1a2461629162106",
+        dataType: "json",
+        data: {
+         q: request.term
+       },
+       success: function( data ) {
+         if(!data.length){
+
+           var result = [
+           {
+            label: 'No matches found', 
+            value: response.term
+          }
+          ];
+
+          response(result);
+        }
+        else {
+         response($.map(data, function(item) {
+
+          if (item.name.match('/,/'))
+          {
+            var valueArr=item.name.split(",");
+            var valueStr=valueArr[0];
+          }
+          else
+          {
+            var valueStr=false;
+          }
+
+           return {
+             label : item.name,
+             value : valueStr
+           };
+         }));
+       }
+
+     }
+   });
+     },
+     select: function (event, ui) {
+      $("#locationInput").val(ui.item.label);
+      $("#locationInput").attr("data-search", ui.item.value);
+      return false;
+    }
+  });
 }
 
 function clearHtml()
@@ -75,7 +90,7 @@ function loadCurrentWeather() {
   if (location)
   {
     var url = 'http://api.apixu.com/v1/current.json?key=513d8003c8b348f1a2461629162106&q='+location+'';
-    var json = loadJSON(url, gotWeather, errorCallback);
+    var json = loadJSON(url, gotCurrentWeather, errorCallback);
 
     clearHtml();
   }
@@ -86,14 +101,31 @@ function loadCurrentWeather() {
   
 }
 
+function loadForecast() {
+    var url = 'http://api.apixu.com/v1/forecast.json?key=513d8003c8b348f1a2461629162106&q='+location+'&days=4';
+    var json = loadJSON(url, gotWeatherForecast, errorCallback2);
+}
+
 function errorCallback(data)
 {
+  console.log(data);
   ready=false;
   background(255);
   noLoop();
 
-  alert("Error: "+data.statusText);
+  alert("Error1: "+data.statusText);
 }
+
+function errorCallback2(data)
+{
+  console.log(data);
+  ready=false;
+  background(255);
+  noLoop();
+
+  alert("Error2: "+data.statusText);
+}
+
 
 function drawArrow()
 {
@@ -121,14 +153,14 @@ function drawInformation()
   rectMode(CORNER);
   noStroke();
 
-  var firstLine=curr_temp+'C, '+curr_location+', '+curr_country;
+  var firstLine=curr_temp+'°, '+curr_location+', '+curr_country;
   var textWidthSize=textWidth(firstLine);
   var colorRect=color(100,100,100);
   
   colorRect.setAlpha(180);
   
   fill(colorRect);
-  rect(0, 0, textWidthSize+50, 120, 0, 20, 20, 0);
+  rect(0, 0, textWidthSize+200, 120, 0, 20, 20, 0);
 
   fill(255);
   textSize(26);
@@ -138,9 +170,30 @@ function drawInformation()
   image(imageIconObject, 12, 45, 64, 64);
 
   fill(colorRect);
-  rect(0,height-65,200,80, 0, 20, 20);
+  rect(0,height-65, 200, 80, 0, 20, 20);
   fill(255);
   text(curr_wind+'MPH', 70, height-25);
+
+  if (forecast.length>0)
+  {
+    fill(colorRect);
+    rect(width/2+20, height-65, 350, 80, 20, 20, 20, 20);
+    fill(255);
+    textSize(16);
+    text(forecast[1].date, 380, height-40);
+    text(forecast[1].avg_temp+"°", 380, height-20);
+
+    text(forecast[2].date, 480, height-40);
+    text(forecast[2].avg_temp+"°", 480, height-20);
+
+    text(forecast[3].date, 580, height-40);
+    text(forecast[3].avg_temp+"°", 580, height-20);
+
+    stroke(60);
+    line(width/2+130, height-65, width/2+130, height);
+    line(width/2+230, height-65, width/2+230, height);
+  }
+ 
 }
 
 function draw() {
@@ -163,7 +216,7 @@ function draw() {
 
 }
 
-function gotWeather(weather) {
+function gotCurrentWeather(weather) {
   if (weather)
   {
     callMethod=true;
@@ -187,6 +240,19 @@ function gotWeather(weather) {
     for (i=0; i<50; i++)
     {
       balls[i]=new Ball();
+    }
+
+    loadForecast();
+  }
+  
+}
+
+function gotWeatherForecast(weather) {
+  if (weather)
+  {
+    for (j=0; j<weather.forecast.forecastday.length; j++)
+    {
+      forecast[j]={date: weather.forecast.forecastday[j].date, avg_temp:weather.forecast.forecastday[j].day.avgtemp_c};
     }
 
     ready=true;
